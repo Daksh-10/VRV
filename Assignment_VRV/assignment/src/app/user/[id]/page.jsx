69eedd1172a04ac627e79daa5c8b5d9f8b206638
Daksh <dakshkaushik20040126@gmail.com>
2024-11-27 18:30:41 +0530

    updated user page

--- Assignment_VRV/assignment/src/app/user/[id]/page.jsx
+++ Assignment_VRV/assignment/src/app/user/[id]/page.jsx
@@ -12,6 +12,27 @@ function User() {
       />
 
       <div>UserName</div>
+
+      <div>
+        <div>Job: </div>
+        <div>Maintainer</div>
+        <div>Permission:</div>
+        <div>Write or Delete</div>
+      </div>
+
+      <div>
+        <textarea>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste nulla,
+          tenetur corrupti corporis, beatae consectetur soluta accusamus
+          molestias tempore cum quos doloribus dignissimos placeat iure rerum
+          fuga odio labore aperiam? At nihil molestiae quibusdam odit nulla
+          quasi minus labore excepturi saepe facere voluptatem atque,
+          necessitatibus modi asperiores facilis quas nemo eligendi quod sequi
+          veritatis non sapiente corporis obcaecati magni? Laudantium.
+        </textarea>
+        <button>Write</button>
+        <button>Delete</button>
+      </div>
     </>
   );
 }
